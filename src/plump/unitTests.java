891241e41ecaeab72f37c78a68d4ade2d117bb21
em222iv <erikper.magnusson@gmail.com>
2015-11-20 13:41:39 +0100

    RED

---
+++ src/plump/unitTests.java
@@ -0,0 +1,64 @@
+package plump;
+import static org.junit.Assert.*;
+import org.mockito.Mockito;
+
+import org.junit.Test;
+
+public class unitTests {
+	private Suit[] s = null;
+	private Value[] v = null;
+	@Test
+	
+	public void shouldSetCardVariableViaConstructorAndGetZero() {
+		Card card = new Card(0);
+		assertEquals(card.getA(),0);
+	}
+	
+	@Test
+	public void shouldExistSuitEnumWithSuitHeartValue()
+	{
+	    s  = Suit.values();
+	    assertEquals(s[0], Suit.HEARTS);
+	}
+	
+	@Test
+	public void shouldExistSuitEnumWithSuitSpadesValue()
+	{
+	    s  = Suit.values();
+	    assertEquals(s[1], Suit.SPADES);
+	}
+	
+	@Test
+	public void shouldExistSuitEnumWithSuitDiamondValue()
+	{
+	    s  = Suit.values();
+	    assertEquals(s[2], Suit.DIAMONDS);
+	}
+	
+	@Test
+	public void shouldExistSuitEnumWithSuitClubsValue()
+	{
+	    s  = Suit.values();
+	    assertEquals(s[3], Suit.CLUBS);
+	}
+	
+	@Test
+	public void shouldExistValueEnumWithValueAce()
+	{
+		 v  = Value.values();
+	     assertEquals(v[0], Value.ACE);
+	}
+	
+	@Test
+	public void shouldExist13DifferentValuesinValueEnum()
+	{
+	     assertEquals(13, Value.values().length);
+	}
+	
+	@Test
+	public void shouldExistConstrunctorInValueEnum()
+	{
+		 v  = Value.values(1);
+	     assertEquals(v[0], Value.ACE);
+	}
+}
---
+++ tests/plumpTests.java
@@ -0,0 +1,61 @@
+package plump;
+import static org.junit.Assert.*;
+import org.mockito.Mockito;
+
+import plump.src.Card;
+import plump.src.Suit;
+import plump.src.Value;
+
+import org.junit.Test;
+
+public class plumpTests {
+	private Suit[] s = null;
+	private Value[] v = null;
+	@Test
+	
+	public void shouldSetCardVariableViaConstructorAndGetZero() {
+		Card card = new Card(0);
+		assertEquals(card.getA(),0);
+	}
+	
+	@Test
+	public void shouldExistSuitEnumWithSuitHeartValue()
+	{
+	    s  = Suit.values();
+	    assertEquals(s[0], Suit.HEARTS);
+	}
+	
+	@Test
+	public void shouldExistSuitEnumWithSuitSpadesValue()
+	{
+	    s  = Suit.values();
+	    assertEquals(s[1], Suit.SPADES);
+	}
+	
+	@Test
+	public void shouldExistSuitEnumWithSuitDiamondValue()
+	{
+	    s  = Suit.values();
+	    assertEquals(s[2], Suit.DIAMONDS);
+	}
+	
+	@Test
+	public void shouldExistSuitEnumWithSuitClubsValue()
+	{
+	    s  = Suit.values();
+	    assertEquals(s[3], Suit.CLUBS);
+	}
+	
+	@Test
+	public void shouldExistValueEnumWithValueAce()
+	{
+		 v  = Value.values();
+	     assertEquals(v[0], Value.ACE);
+	}
+	
+	@Test
+	public void shouldExist13DifferentValuesinValueEnum()
+	{
+	     assertEquals(13, Value.values().length);
+	}
+}
