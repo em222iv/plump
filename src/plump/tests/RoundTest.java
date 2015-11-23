73aa68e756b1337ddd225702bc3ba898f4cd9edf
em222iv <erikper.magnusson@gmail.com>
2015-11-23 16:05:37 +0100

    RED

--- src/plump/tests/RoundTest.java
+++ src/plump/tests/RoundTest.java
@@ -1,16 +1,35 @@
 package plump.tests;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import plump.Deck;
+import plump.Game;
+import plump.Player;
 import plump.Round;
 
 public class RoundTest {
-
+	
+	private Game gameMock;
+ 	private Player playerMock;
+		 
+	@Before
+	public void setUp() {
+	  gameMock = mock( Game.class );
+	  playerMock = mock( Player.class );
+	}
+	   
 	@Test
 	public void shouldSendFreshDeckIntoRound() {
 		Round round = new Round(new Deck());
 	}
 
+	@Test
+	public void test() {
+		Game game = new Game(playerMock,5,new Deck());
+		Round round = new Round(game.genereateAI(), new Deck());
+	}
 }
