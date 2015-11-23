039035986ed084ef022575654945778c4b60a689
em222iv <erikper.magnusson@gmail.com>
2015-11-23 13:11:04 +0100

    GREEN

--- src/plump/Game.java
+++ src/plump/Game.java
@@ -1,14 +1,15 @@
 package plump;
 
 public class Game {
-	
+	int rounds = 0;
 	Player player;
 	public Game(Player _player) {
 		this.player = _player;
 	}
 	
 	public Game(Player _player,int rounds) {
-//		this.player = _player;
+		this.player = _player;
+		this.rounds = rounds;
 	}
 	
 }
