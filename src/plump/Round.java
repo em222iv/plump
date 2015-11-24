9336d7ce92497392fa0a21c4e7e84cdb66aba1b2
em222iv <erikper.magnusson@gmail.com>
2015-11-24 11:56:42 +0100

    GREEN

--- src/plump/Round.java
+++ src/plump/Round.java
@@ -3,7 +3,10 @@ package plump;
 import java.util.ArrayList;
 
 public class Round {
+	public Deck deck;
 	public Round(ArrayList<Player> players,Deck deck,int round) {
+		this.deck = deck;
+		
 	}
 }
  
