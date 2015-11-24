ac3a7d9fc1857a90d58cb0e553211859d4bb6267
em222iv <erikper.magnusson@gmail.com>
2015-11-24 11:57:07 +0100

    REFACTOR

--- src/plump/Round.java
+++ src/plump/Round.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 
 public class Round {
 	public Deck deck;
-	public Round(ArrayList<Player> players,Deck deck,int round) {
-		this.deck = deck;
+	public Round(ArrayList<Player> players,Deck deck_,int round) {
+		this.deck = deck_;
 		
 	}
 }
