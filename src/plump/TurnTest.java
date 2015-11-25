e6ed087a6e7a74d500eafd84eb02e1f88528fca8
em222iv <erikper.magnusson@gmail.com>
2015-11-25 12:59:15 +0100

    REFACTOR

--- src/plump/TurnTest.java
+++ src/plump/TurnTest.java
@@ -1,13 +1,34 @@
 package plump;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
 
+import java.util.ArrayList;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class TurnTest {
-
+	
+	private Deck deckMock;
+	private Game gameMock;
+ 	private Player playerMock;
+ 	private ArrayList<Player> pList;
+		 
+	@Before
+	public void setUp() {
+	  gameMock = mock( Game.class );
+	  playerMock = mock( Player.class );
+	  deckMock = mock( Deck.class );
+	  pList = new ArrayList<Player>();
+	  pList.add(new Player("player1", new Deck()));
+	  pList.add(new Player("Player2", new Deck()));
+	  pList.add(new Player("Player3", new Deck()));
+      pList.add(new Player("Player4", new Deck()));
+	}
+	
 	@Test
-	public void test() {
+	public void shouldExistsTurnConstructor() {
 		new Turn(null);
 	}
 
